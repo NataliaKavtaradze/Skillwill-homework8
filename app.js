a051813eb55e1d5ebbcc2c9120bc871aa757fa87
Natalia Kavtaradze <nataliakavtaradze777@gmail.com>
2025-11-25 01:18:29 +0400

    finished first part

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-console.log("ok")
+function expo(num, power, cb){
+  if (power ===0){
+    return cb(null, 1);
+  }
+
+  expo(num, power -1, function(err, result){
+  if(err)return cb(err, null);
+  cb(null, num * result);
+});
+}
+expo(5,3, function(err, result){
+  if(err) return console.log(err);
+  console.log(result);
+});
+
+
    
