de5e0d60941c7d890415f86740ef9b2e9db9a76d
Natalia Kavtaradze <nataliakavtaradze777@gmail.com>
2025-11-25 01:54:22 +0400

    finished homework

--- app.js
+++ app.js
@@ -12,27 +12,61 @@
 //   if(err) return console.log(err);
 //   console.log(result);
 // });
-
-
-fetch("https://jsonplaceholder.typicode.com/posts")
-.then(responce => responce.json())
-.then(posts => {
-  const container = document.getElementById("posts");
-
-  posts.forEach(post => {
-    const div = document.createElement("div");
-    div.classList.add("post");
  
-    div.innerHTML = `
-    <h3>${post.title}</h3>
-     <p>${post.body}</p>
-     <hr/>
-    `
-     container.appendChild(div);
-  });
-  })
-  .catch(error => console.error("Error:", error));
+           //fetch
 
+// fetch("https://jsonplaceholder.typicode.com/posts")
+// .then(responce => responce.json())
+// .then(posts => {
+//   const container = document.getElementById("posts");
+
+//   posts.forEach(post => {
+//     const div = document.createElement("div");
+//     div.classList.add("post");
+ 
+//     div.innerHTML = `
+//     <h3>${post.title}</h3>
+//      <p>${post.body}</p>
+//      <hr/>
+//     `
+//      container.appendChild(div);
+//   });
+//   })
+//   .catch(error => console.error("Error:", error));
+
+
+async function deepCopyAsync(obj) {
+  return new Promise((resolve, reject) => {
+        if (typeof obj !== "object") {
+      return reject("Argument must be an object");
+    }
+
+    try {
+      const copy = JSON.parse(JSON.stringify(obj));
+      resolve(copy);
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
+
+const original = {
+  name: "Gio",
+  details: {
+    age: 25,
+    skills: ["JS", "React"]
+  }
+};
+
+deepCopyAsync(original)
+  .then(copy => {
+    console.log("Copied object:", copy);
+  })
+  .catch(err => {
+    console.error("Error:", err);
+  });
+
+  deepCopyAsync(7);
 
 
     
